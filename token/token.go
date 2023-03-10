b254cfcb671896514f01803e474a634456f08930
MariaProcopii <mari.procopii@gmail.com>
2023-03-10 18:11:09 +0200

    ready. set. go

---
+++ token/token.go
@@ -0,0 +1,84 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT  = "IDENT"
+	INT    = "INT"
+	BOOL   = "BOOL"
+	NONE   = "NONE"
+	STRING = "STRING"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ        = "=="
+	NOT_EQ    = "!="
+	AND       = "&&"
+	OR        = "||"
+	INCREMENT = "++"
+	DECREMENT = "--"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	DBLQUOTE  = "\""
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	LSQBRA    = "["
+	RSQBRA    = "]"
+	NEWLINE   = "\n"
+
+	// Comments
+	COMMENT = "//"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	UNLESS   = "UNLESS"
+	THEN 	 = "THEN"
+	ELSE     = "ELSE"
+	FOR      = "FOR"
+	RETURN   = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"def":    FUNCTION,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"unless": UNLESS,
+	"then":   THEN,
+	"else":   ELSE,
+	"for":    FOR,
+	"return": RETURN,
+	"none":   NONE,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
